bf69bbd906d41a4ddad2f8176e605ce36f300b77
aahanaw13 <122175611+aahanaw13@users.noreply.github.com>
2025-08-09 18:02:20 +0000

    created add, toggle function

--- TodoList/app/(tabs)/index.tsx
+++ TodoList/app/(tabs)/index.tsx
@@ -45,15 +45,62 @@ loadTodos(database);
   }
   catch(error){
     console.error("database initialization failed:", error);
-    Alert.alert("error", "failed to load todos");
+    Alert.alert("error", "failed to initialize database");
   }
 };
 const loadTodos=async(database=db)=>{
   if(!database) return;
-  try{}
+  try{
+    const results= await database.executeSql('SELECT * FROM todos ORDER BY created_at DESC');
+    const rows= results[0].rows;
+    const todoList=[];
+    for(let i=0; i<rows.length; i++){
+      todoList.push(rows.item(i));
+    }
+    setTodos(todoList);
+
+  }
   catch(error){
     console.error("failed to load todos:", error);
     Alert.alert("error", "failed to load todos");
+  }
+}
+const addTodo=async ()=>{
+  if (! newTodo.trim()){
+    Alert.alert('error','please enter a todo item');
+    return
+  };
+  if(! db){
+    Alert.alert('error',"database not initialized");
+    return;
+  }
+  try {
+    await db.executeSql(
+      'INSERT INTO todos (text, completed) VALUES (?,?)',
+      [newTodo.trim(),0]
+    );
+    setNewTodo('');
+    loadTodos();
+    console.log('todo edit successfully');
+  }
+  catch(error){
+    console.error("failed to add todo", error);
+    Alert.alert("error", "failed to add todo");
+  }
+}
+const toggleTodo=async(id, currentStatus)=>{
+  if (! db) return;
+  try{
+    await db.executeSql(
+      'UPDATE todos SET completed=? WHERE id=?',
+      [currentStatus ? 0:1, id]
+    );
+    loadTodos();
+    console.log('todo status updated');
+  }
+  catch(error){
+    console.error("failed to update todo", error);
+    Alert.alert("error", "failed to update todo");
   }
 }
 }
